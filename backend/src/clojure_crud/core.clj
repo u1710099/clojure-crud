49ca330dc4f929bd91626ed403d0878f6fd90644
Gulbahar Mirzalieva <gulbakhormirzalieva99@gmail.com>
2025-04-07 01:16:46 +0500

    add: frontend ui

--- backend/src/clojure_crud/core.clj
+++ backend/src/clojure_crud/core.clj
@@ -1,47 +1,17 @@
 (ns clojure_crud.core
-  (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.json :as middleware]
-            [clojure.data.json :as json]))
+  (:require [ring.middleware.cors :refer [wrap-cors]]
+            [ring.adapter.jetty :as jetty]))
 
-;; In-memory database
-(defonce db (atom {:items {1 {:id 1 :name "Sample" :value 5}}}))
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body "{\"message\":\"Hello from backend\"}"})
 
-;; CRUD Handlers
-(defn create-item [item]
-  (let [id (inc (apply max (keys (:items @db))))]
-    (swap! db assoc-in [:items id] (assoc item :id id))))
-
-(defn read-items []
-  (vals (:items @db)))
-
-(defn update-item [id updates]
-  (swap! db update-in [:items id] merge updates))
-
-(defn delete-item [id]
-  (swap! db update :items dissoc id))
-
-;; HTTP Handlers
-(defn app [request]
-  (case [(:request-method request) (:uri request)]
-    [:get "/items"] {:status 200
-                     :headers {"Content-Type" "application/json"}
-                     :body (json/write-str (read-items))}
-
-    [:post "/items"] (let [item (json/read-str (slurp (:body request)) :key-fn keyword)]
-                       (create-item item)
-                       {:status 201 :body "Created"})
-
-    [:put "/items/:id"] (let [id (-> request :params :id Integer/parseInt)
-                              updates (json/read-str (slurp (:body request)) :key-fn keyword)]
-                          (update-item id updates)
-                          {:status 200 :body "Updated"})
-
-    [:delete "/items/:id"] (let [id (-> request :params :id Integer/parseInt)]
-                             (delete-item id)
-                             {:status 204 :body ""})
-
-    {:status 404 :body "Not Found"}))
+(def app
+  (-> handler
+      (wrap-cors
+        :access-control-allow-origin [#"http://localhost:8080"]
+        :access-control-allow-methods [:get :post :put :delete])))
 
 (defn -main []
-  (jetty/run-jetty (middleware/wrap-json-body app {:keywords? true})
-                   {:port 3000 :join? false}))
+  (jetty/run-jetty app {:port 3000}))
--- frontend/src/clojure_crud/core.cljs
+++ frontend/src/clojure_crud/core.cljs
@@ -1,80 +1,17 @@
-(ns clojure_crud.core
-  (:require [reagent.core :as r]
-            [reagent.dom :as rdom]
-            [cljs-http.client :as http]
-            [cljs.core.async :refer [<! go]]))
+(ns clojure-crud.core
+  (:require [cljs-http.client :as http]
+            [reagent.dom :as rdom]))
 
-;; State
-(defonce state (r/atom {:items []
-                        :new-item ""
-                        :editing nil}))
+(defn api-test []
+      (http/get "http://localhost:3000/api"
+                {:with-credentials? false}
+                #(js/console.log "Success:" %)
+                #(js/console.error "Error:" %)))
 
-;; API Calls
-(defn fetch-items! []
-      (go (let [response (<! (http/get "http://localhost:3000/items"))]
-               (swap! state assoc :items (:body response)))))
+(defn app []
+      [:div
+       [:h1 "Frontend"]
+       [:button {:on-click api-test} "Call Backend API"]])
 
-(defn create-item! [name value]
-      (go (<! (http/post "http://localhost:3000/items"
-                         {:json-params {:name name :value value}}))
-          (fetch-items!)))
-
-(defn update-item! [id name value]
-      (go (<! (http/put (str "http://localhost:3000/items/" id)
-                        {:json-params {:name name :value value}}))
-          (fetch-items!)))
-
-(defn delete-item! [id]
-      (go (<! (http/delete (str "http://localhost:3000/items/" id)))
-          (fetch-items!)))
-
-;; Components
-(defn item-form []
-      (let [name (r/atom "")
-            value (r/atom 0)]
-           (fn []
-               [:div.form
-                [:input {:type "text" :value @name
-                         :on-change #(reset! name (-> % .-target .-value))}]
-                [:input {:type "number" :value @value
-                         :on-change #(reset! value (-> % .-target .-value int))}]
-                [:button {:on-click #(do (create-item! @name @value)
-                                         (reset! name "")
-                                         (reset! value 0))}
-                 "Add Item"]])))
-
-(defn item-list []
-      [:div.items
-       (for [{:keys [id name value]} (:items @state)]
-            ^{:key id}
-            [:div.item
-             (if (= id (:editing @state))
-               [:div
-                [:input {:default-value name
-                         :on-blur #(update-item! id (-> % .-target .-value) value)}]
-                [:input {:type "number" :default-value value
-                         :on-blur #(update-item! id name (-> % .-target .-value int))}]]
-               [:div
-                [:span name " (" value ")"]
-                [:button {:on-click #(swap! state assoc :editing id)} "✏️"]
-                [:button {:on-click #(delete-item! id)} "🗑️"]])])
-
-       (defn bar-chart []
-             [:div.chart
-              (for [{:keys [name value]} (:items @state)]
-                   ^{:key name}
-                   [:div.bar-container
-                    [:div.bar-label name]
-                    [:div.bar {:style {:height (str (* 5 value) "px")}}
-                     value]])])
-
-       (defn app []
-             [:div
-              [:h1 "CRUD Visualization"]
-              [item-form]
-              [item-list]
-              [bar-chart]])
-
-       ;; Initialize
-       (defn ^:export init []
-             (rdom/render [app] (js/document.getElementById "app"))(fetch-items!))])
+(defn ^:export init []
+      (rdom/render [app] (js/document.getElementById "app")))
